16936cefad71112058a0f84a428ee81d81fb3fb9
sumbuz28 <psumbureru@gmail.com>
2014-07-12 16:30:46 +0530

    plot 3 code

---
+++ plot3.R
@@ -0,0 +1,10 @@
+read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt"'),header=F, sep=';')
+colnames(dat) <-names(read.table('household_power_consumption.txt', header=TRUE,sep=";",nrows=1))
+dat$dateTime <- strptime( paste(dat$Date,dat$Time), format="%d/%m/%Y %H:%M:%S")
+dev.copy(png,file="plot3.png", width=480,height=480)
+plot (dat[,10],dat[,7],type = "l", xlab = "", ylab = "Energy sub metering", col = "black")
+lines (dat[,10],dat[,8],type = "l", col = "red")
+points (dat[,10],dat[,9],type = "l", col = "blue")
+legend("topright", lty = 1,inset=0.05, col = c("black", "red", "blue"), legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
+dev.off()
+

672eb7af558d6e7e2a290c78120449f8b9afa460
sumbuz28 <psumbureru@gmail.com>
2014-07-12 16:28:12 +0530

    plot 2 code

---
+++ plot2.R
@@ -0,0 +1,8 @@
+read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt"'),header=F, sep=';')
+colnames(dat) <-names(read.table('household_power_consumption.txt', header=TRUE,sep=";",nrows=1))
+
+dat$dateTime <- strptime( paste(dat$Date,dat$Time), format="%d/%m/%Y %H:%M:%S")
+plot(dat[,10],dat[,3],type="l",ylab="Global Active Power(kilowats)",xlab="")
+dev.copy(png,file="plot2.png", width=480, height=480)
+dev.off()
+

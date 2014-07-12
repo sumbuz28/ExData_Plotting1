2179dfcb5f0caa7790c323ceef1a2b72c60f2698
sumbuz28 <psumbureru@gmail.com>
2014-07-12 16:47:37 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,4 +1,4 @@
-read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt"'),header=F, sep=';')
+dat<-read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt"'),header=F, sep=';')
 colnames(dat) <-names(read.table('household_power_consumption.txt', header=TRUE,sep=";",nrows=1))
 dat$dateTime <- strptime( paste(dat$Date,dat$Time), format="%d/%m/%Y %H:%M:%S")
 dev.copy(png,file="plot3.png", width=480,height=480)
